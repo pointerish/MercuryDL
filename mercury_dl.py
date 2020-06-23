dcb0ffd29b506dc90c94a6402036e3c591e26273
Josías Alvarado Gutiérrez <josiasjag@gmail.com>
2020-06-22 18:12:56 -0600

    Update

--- mercury_dl.py
+++ mercury_dl.py
@@ -10,11 +10,13 @@ class YTDLSession:
         self.session_id = hashlib.sha256(str.encode(session_user)).hexdigest()
 
     def __str__(self):
-        return f'YTDLSession({self.params}, {self.session_user}) with Session ID <{self.session_id}>'
+        return f'YTDLSession("{self.params}", "{self.session_user}") with Session ID <{self.session_id}>'
 
     def _download(self):
         download_args = shlex.split(self.params)
         if download_args[0] == 'youtube-dl':
-            subprocess.run(download_args) # Need to make sure this is safe!
+            execution = subprocess.check_output(download_args) # Need to make sure this is safe!
+            file_name = execution.decode('utf-8').split('[ffmpeg] Destination: ')[1].split('\n')[0]
+            return {'msg':'Success!', 'file':file_name}
         else:
-            return "Non Youtube-DL command not allowed. Use a valid Youtube-DL command."
+            return {'msg':'Non Youtube-DL command not allowed. Use a valid Youtube-DL command.'}
