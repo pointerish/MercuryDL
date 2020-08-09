d99cc59d7c4e0b2c9ff641eb71b980db36e2c840
Josías Alvarado <pointerish@pop-os.localdomain>
2020-08-08 22:16:15 -0600

    Added session storing functionality

--- mercury_dl.py
+++ mercury_dl.py
@@ -1,14 +1,18 @@
+import json
 import hashlib
-import youtube_dl
 import shlex, subprocess
 from utils import is_safe
 
 class YTDLSession:
 
-    def __init__(self, params : str, session_user : str):
+    """
+    This class contains the YoutubeDL methods needed to download the files.
+    """
+
+    def __init__(self, params : str, api_token : str) -> None:
         self.params = params
-        self.session_user = session_user
-        self.session_id = hashlib.sha256(str.encode(session_user)).hexdigest()
+        self.api_token = api_token
+        self.ses_token = hashlib.sha256(str.encode(self.api_token)).hexdigest()
 
     def __str__(self) -> str:
         return f'YTDLSession("{self.params}", "{self.session_user}") with Session ID <{self.session_id}>'
@@ -20,7 +24,11 @@ class YTDLSession:
             file_name = execution.decode('utf-8').split('[ffmpeg] Destination: ')[1].split('\n')[0]
             return {'msg':'Success!', 'file':file_name}
         else:
-            return {'msg':'Non Youtube-DL command/option not allowed. Use a valid Youtube-DL command/option.'}
+            return {'msg':'Non Youtube-DL \
+                     command/option not allowed. \
+                     Use a valid Youtube-DL command/option.',
+                     'file':'null'}
 
-    def store_session(self):
-        pass
+    def _store_session(self) -> None:
+        with open('sessions.json', 'w') as sessions:
+            json.dump({self.ses_token:hashlib.sha256(str.encode(self.params)).hexdigest()}, sessions)
